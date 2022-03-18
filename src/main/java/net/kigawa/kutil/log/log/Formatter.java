b44bdef2c916a06d0294e5524c6e1125502ce129
kigawa <kigawa.8390@gmail.com>
2022-03-18 11:55:26 +0900

    fix: formatter

--- src/main/java/net/kigawa/kutil/log/log/Formatter.java
+++ src/main/java/net/kigawa/kutil/log/log/Formatter.java
@@ -5,13 +5,14 @@ import java.util.logging.LogRecord;
 
 public class Formatter extends java.util.logging.Formatter {
     private final Calendar calendar = Calendar.getInstance();
+
     @Override
     public String format(LogRecord record) {
         StringBuffer sb = new StringBuffer();
         calendar.setTimeInMillis(record.getMillis());
         sb.append(Calendar.MONTH).append("-").append(Calendar.DAY_OF_MONTH).append("-").append(Calendar.HOUR_OF_DAY)
-                .append("\n").append("-").append(Calendar.MINUTE).append("-").append(Calendar.SECOND).append("[")
-                .append(record.getLevel().getName()).append("] |").append(record.getMessage());
+                .append("-").append(Calendar.MINUTE).append("-").append(Calendar.SECOND).append("[")
+                .append(record.getLevel().getName()).append("] |").append(record.getMessage()).append("\n");
         return sb.toString();
     }
 }
